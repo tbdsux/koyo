1e8c28ab43e1aaa62ae432939d963102915d0585
TheBoringDude <iamcoderx@gmail.com>
2022-12-03 23:53:39 +0800

    simple api for screenshots

--- api/index.js
+++ api/index.js
@@ -1,9 +1,49 @@
 const express = require('express');
+const chromium = require('@sparticuz/chromium');
+const puppeteer = require('puppeteer');
+
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.post('/screenshot', async (req, res) => {
+	const { website } = req.body;
+	if (!website) {
+		res.status(400).json({ error: true, message: 'Website url not defined in body.' });
+		return;
+	}
+
+	try {
+		const browser = await puppeteer.launch({
+			args: chromium.args,
+			defaultViewport: chromium.defaultViewport,
+			executablePath: await chromium.executablePath,
+			headless: chromium.headless,
+			ignoreHTTPSErrors: true
+		});
+		const page = await browser.newPage();
+		await page.goto(website, { waitUntil: 'domcontentloaded' });
+
+		const data = await page.screenshot({ encoding: 'base64' });
+		const base64Data = data.replace(/^data:image\/png;base64,/, '');
+		const img = Buffer.from(base64Data, 'base64');
+
+		await browser.close();
+
+		res.writeHead(200, {
+			'Content-Type': 'image/png',
+			'Content-Length': img.length
+		});
+		res.end(img);
+	} catch (e) {
+		res.status(500).json({ error: true, message: String(e) });
+	}
 });
 
 app.listen(port, () => {
